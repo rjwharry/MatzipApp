7f9c84fd0a26c235877e516082bf7221a1d5949b
rjwharry <rjwharry2003@gmail.com>
2025-01-28 21:31:37 +0900

    make custom InputField component and login screen

---
+++ front/src/components/InputField.tsx
@@ -0,0 +1,68 @@
+import React, { useRef } from 'react';
+import { Dimensions, Pressable, StyleSheet, TextInput, TextInputProps, View } from 'react-native';
+import { colors } from '../constants';
+import { Text } from 'react-native-gesture-handler';
+
+interface InputFieldProps extends TextInputProps {
+    disabled?: boolean; 
+    error?: string;
+    touched?: boolean
+}
+
+const deviceHeight = Dimensions.get('screen').height
+
+const InputField = ({disabled = false, error, touched, ...props }: InputFieldProps) => {
+    const innerRef = useRef<TextInput | null>(null);
+
+    const handlePressInput = () => {
+        innerRef.current?.focus();
+    }
+    return (
+        <Pressable onPress={handlePressInput}>
+            <View style={[
+                styles.container, 
+                disabled && styles.disabled, 
+                touched && Boolean(error) && styles.inputError
+            ]}>
+                <TextInput
+                    ref={innerRef}
+                    editable={!disabled}
+                    style={[styles.input]} 
+                    placeholderTextColor={colors.GRAY_500}
+                    autoCapitalize='none'
+                    spellCheck={false}
+                    autoCorrect={false}
+                    {...props}
+                />
+                {touched && Boolean(error) && <Text style={styles.errorText}>{error}</Text>}
+            </View>
+        </Pressable>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth: 1,
+        borderColor: colors.GRAY_200,
+        padding: deviceHeight > 700 ? 15: 10,
+    },
+    input: {
+        fontSize: 16,
+        color: colors.BLACK,
+        padding: 0,
+    },
+    disabled: {
+        backgroundColor: colors.GRAY_200,
+        color: colors.GRAY_700,
+    },
+    inputError: {
+        borderWidth: 1,
+        borderColor: colors.RED_300,
+    },
+    errorText: {
+        color: colors.RED_500,
+        fontSize: 12,
+    }
+})
+
+export default InputField;
--- front/src/screens/auth/LoginScreen.tsx
+++ front/src/screens/auth/LoginScreen.tsx
@@ -1,13 +1,67 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import InputField from '../../components/InputField';
 
 const LoginScreen: React.FC = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [touched, setTouched] = useState({
+        email: false,
+        password: false
+    });
+
+    const handleChangeText = (key: string, value: string) => {
+        setValues({
+            ...values,
+            [key]: value
+        });
+    }
+
+    const handleBlur = (key: string) => {
+        setTouched({
+            ...touched,
+            [key]: true
+        });
+    }
+
     return (
-        <SafeAreaView>
-            <Text>Login</Text>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.inputContainer}>
+                <InputField 
+                    placeholder='이메일'
+                    error="이메일을 입력해주세요" 
+                    inputMode='email'
+                    touched={touched.email} 
+                    value={values.email}
+                    onChangeText={(text) => handleChangeText('email', text)}
+                    onBlur={() => handleBlur('email')}
+                />
+                <InputField 
+                    placeholder='비밀번호' 
+                    error='비밀번호를 입력해주세요'
+                    secureTextEntry
+                    touched={touched.password}
+                    value={values.password}
+                    onChangeText={(text) => handleChangeText('password', text)}
+                    onBlur={() => handleBlur('password')}
+                />
+            </View>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        margin: 30,
+    },
+    inputContainer: {
+        gap: 20
+    }
+});
 
 export default LoginScreen;
